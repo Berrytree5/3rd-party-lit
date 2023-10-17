1277589bf418ba1046cb2815a83e0060f2312bc5
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-16 20:47:54 -0700

    Fixed issue w/ css file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 3rd-party-lit
+The assignment was to create a calendar w/ the code provided.
+Fixed issue with the boxes not staying in-line.
+Used the Day.js function to get current date/time.
+added various comments for easy to follow code.
+![alt text describing the image](./assets/images/Screen%20Shot%202023-10-16%20at%208.36.14%20PM.png)
+https://github.com/Berrytree5/3rd-party-lit.git
--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+const presentTime = dayjs();
+const currentTime = document.getElementById("currentDay");
+const saveBtns = document.querySelectorAll(".saveBtn");
+
+
+saveBtns.forEach((saveBtn) => {
+  saveBtn.addEventListener("click", saveTime);
 });
+
+// Display the current date
+displayDate();
+
+// Set text values for the time blocks
+setTextValue();
+
+// Apply color coding to time blocks
+timeBlockColor();
+
+// Function to save user input to local storage
+function saveTime(event) {
+  const textArea = event.target.previousElementSibling;
+  const time = event.target.parentElement.id;
+  localStorage.setItem(time, textArea.value);
+}
+
+// Function to apply color coding to time blocks
+function timeBlockColor() {
+  const currentHour = presentTime.hour();
+
+  document.querySelectorAll(".time-block").forEach((timeBlock) => {
+    const timeBlockHour = parseInt(timeBlock.id);
+    if (timeBlockHour < currentHour) {
+      timeBlock.classList.add("past");
+    } else if (timeBlockHour === currentHour) {
+      timeBlock.classList.add("present");
+    } else {
+      timeBlock.classList.add("future");
+    }
+  });
+}
+
+// Function to set text values for time blocks from local storage
+function setTextValue() {
+  document.querySelectorAll(".time-block").forEach((timeBlock) => {
+    const textArea = timeBlock.querySelector(".description");
+    const time = timeBlock.id;
+    textArea.value = localStorage.getItem(time);
+  });
+}
+
+// Function to display the current date
+function displayDate() {
+  currentTime.textContent = presentTime.format("dddd, MMMM D, YYYY");
+}
